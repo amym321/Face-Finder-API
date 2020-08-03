95a07f8294dba666909f2b41ffb6a3b8ec9cf999
amym321 <66137093+amym321@users.noreply.github.com>
2020-08-03 10:18:11 -0500

    adding port

--- server.js
+++ server.js
@@ -35,6 +35,6 @@ app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db) })
 app.put('/image', (req, res) => {image.handleImage(req, res, db)})
 app.post('/imageUrl', (req, res) => {image.handleApiCall(req, res)})
 
-app.listen(3000, () => {
-    console.log('app is runing on 3000');
+app.listen(process.env.PORT || 3000, () => {
+    console.log('app is runing on ${process.env.PORT}');
 })
